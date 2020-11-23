964d70a99630c48d524c4be2d2bb21ae657a4660
Blake Runyon <blakerunyon09@gmail.com>
2020-11-23 23:54:44 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,3 +1,3 @@
 def meal_choice(broccoli, macaroni, meat="tofu")
-  return puts "A plate of #{meat} with #{broccoli} and #{macaroni}."
+  return "A plate of #{meat} with #{broccoli} and #{macaroni}."
 end
